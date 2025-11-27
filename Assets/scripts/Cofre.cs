fb3edd46d8b044929e9fab3b5bbe85ab8d89bd8c
augusto.mattioda <augustomattioda@gmail.com>
2025-11-27 19:23:09 -0300

    Interaccion + cofre

--- Assets/scripts/Cofre.cs
+++ Assets/scripts/Cofre.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Cofre : MonoBehaviour
+public class Chest : interaccion
 {
-    // Start is called before the first frame update
-    void Start()
+    public Item[] itemsInside;
+    private bool isOpen = false;
+
+    public override void Interact()
     {
-        
+        if (!isOpen)
+        {
+            isOpen = true;
+            Debug.Log("Cofre abierto!");
+            ShowItems();
+        }
+        else
+        {
+            Debug.Log("El cofre ya está abierto.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void ShowItems()
     {
-        
+        foreach (var item in itemsInside)
+        {
+            Debug.Log("Item obtenido: " + item.itemName);
+        }
     }
 }
--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 public class player : entity, iaffectedspeed
 {
-    
+
     [Header("Physics")]
     [SerializeField] private float _jumpforce = 5.0f;
-   
+
     [Header("input")]
     [SerializeField] private KeyCode jumpkey = KeyCode.Space;
 
@@ -27,34 +27,59 @@ public class player : entity, iaffectedspeed
 
     public override void die()
     {
-      
+
     }
-  
+
 
     private void Start()
     {
-       playermovement = new movement(transform, _movespeed, _rb, _jumpforce, _animation, _isgrounded);
-       playerdirection = new direction(playermovement, _animation);
+        playermovement = new movement(transform, _movespeed, _rb, _jumpforce, _animation, _isgrounded);
+        playerdirection = new direction(playermovement, _animation);
     }
 
     private void Update()
     {
-       playerdirection.onUpdate();
+        playerdirection.onUpdate();
+        DetectInteractable();
+
+        if (Input.GetKeyDown(KeyCode.E) && currentInteractable != null)
+        {
+            currentInteractable.Interact();
+        }
     }
     private void FixedUpdate()
     {
-       playerdirection.onfixedupdate(); 
+        playerdirection.onfixedupdate();
     }
 
     public void takeSpeed(float spd)
     {
-      
+
     }
-    
+
 
     public void restoreSpeed(float spd)
     {
-        
+
     }
-    
+
+
+    public float interactionRange = 3f;
+    private interaccion currentInteractable;
+
+    void DetectInteractable()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, interactionRange))
+        {
+            currentInteractable = hit.collider.GetComponent<Interactable>();
+        }
+        else
+        {
+            currentInteractable = null;
+        }
+    }
 }
+
+
